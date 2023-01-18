741a6e9161fd1f69338d37f2fc3aa0a4953dc963
cesar128 <ceterre@hotmail.com>
2023-01-18 01:30:45 -0400

    Working with nginx in the backend to serve media

--- virtualcatalog/settings.py
+++ virtualcatalog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 
